5cab9d048f9d228d864797c1332e87a0ad1dfcdb
Rich <choonforge@firebird.co.nz>
2025-12-01 10:37:07 +1300

    Tidy up documentation Create /src/aimate.web.ui/master tracking doc

--- src/AiMate.Shared.Dtos.Feedback/UpdateFeedbackRequest.cs
+++
@@ -1,9 +0,0 @@
-public class UpdateFeedbackRequest
-{
-    public Guid UserId { get; set; } // Add this property
-    public int? Rating { get; set; }
-    public string? TextFeedback { get; set; }
-    public List<FeedbackTagDto>? Tags { get; set; }
-    public string? ModelId { get; set; }
-    public long? ResponseTimeMs { get; set; }
-}
